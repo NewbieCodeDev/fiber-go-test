890d7e8c780f0bd72b94a188e385e6242c655d97
NewbieCodeDev <tatip.pasinee@gmail.com>
2024-03-19 00:50:04 +0700

    create GET API WITHOUT FIber

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import ("fmt"
+		"net/http"
+		"log")
+
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello World!")
+}
+
+func main() {
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
