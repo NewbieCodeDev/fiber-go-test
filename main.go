bfecbc1678f820768b57c2047e05a633353e09da
NewbieCodeDev <tatip.pasinee@gmail.com>
2024-03-19 01:03:54 +0700

    use fiber

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"log")
+import ("github.com/gofiber/fiber/v2")
 
 
+// fiber is framework like express.js
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/hello",func(c *fiber.Ctx) error{
+		return c.SendString("hello World!")
+	})
+
+	app.Listen(":8080")
+}
+
+
+/* code before use Fiber
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -19,6 +31,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	
 	http.HandleFunc("/hello",helloHandler)
 
 	fmt.Println("Starting server at port 8080")
@@ -26,3 +40,5 @@ func main() {
 		log.Fatal(err)
 	}
 }
+*/
+
